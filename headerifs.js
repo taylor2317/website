ea50718902617b1bec4a1eae617a628826d6c70a
taylor.2317 <71384906+taylor2317@users.noreply.github.com>
2024-06-11 18:27:22 +0100

    footer done, header not done
    footer is all done.
    
    header is being done currently only on homepage

---
+++ headerifs.js
@@ -0,0 +1,10 @@
+let current = window.location.href
+alert(current.slice(-9))
+
+if (current.slice(-9)=="home.html"){
+    homeNav.className += " active"
+
+}
+else if (current.slice(-9)=="home.html"){
+    homeNav.className += " active"
+}
