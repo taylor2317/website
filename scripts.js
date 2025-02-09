2a8aca47f996928be3b8bc742fa1204de92a2a47
taylor.2317 <71384906+taylor2317@users.noreply.github.com>
2025-02-09 20:26:02 +0000

    On-mass update
    Uploading all updates as off 09/02/25
    
    Signed-off-by: taylor.2317 <71384906+taylor2317@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -2,4 +2,4 @@ function menu() {
     document.querySelectorAll('.menuList').forEach(element => {
         element.style.display = (element.style.display === 'none' || element.style.display === '') ? 'block' : 'none';
     });
-};
+}
